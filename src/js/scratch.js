0f0134ea862e12f24cb6052ec29f3360feb9ea18
carlitoa <carlitoa@gmail.com>
2024-09-06 10:49:32 -0400

    code updates

---
+++ src/js/scratch.js
@@ -0,0 +1,12 @@
+btn.parentElement.classList.add("active");
+
+document.getElementsByClassName('Unity_instruction2');
+
+document.getElementsByClassName('Unity_instruction2').classList.add('titleHeader');
+
+
+$('.Unity_instruction2').parent('td').addClass('titleHeader');
+
+$(function() {
+  $('.Unity_h2').parent('td').addClass('titleHeader');
+});
